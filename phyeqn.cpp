9f85821c9b32af7418dccbf9ce6190d24493fcfa
eceryanvij <108124140@nitt.edu>
2024-09-30 17:23:11 +0530

    WEEK 1

---
+++ phyeqn.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	int u = 12, a = 20, t = 20, b = 5, c = 12, p = 16;
+	print("%d", u + a*t);
+	print("%d", u*t + (a*t*t)/2);
+	print("%f", 2*a + pow())
+}
---
+++ swapnumwithoutvariable.cpp
@@ -0,0 +1,10 @@
+//Swap two numbers without a variable
+
+#include <stdio.h>
+
+int main(){
+	int a = 10, b = 20;
+	
+	
+	
+}
